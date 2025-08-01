6d708a42af1e7416de2c990c2a035161c78d5a9d
Krishna yadav <devy3260@gmail.com>
2025-08-01 14:51:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Placeorder from './pages/Placeorder'
 import Order from './pages/Order'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Searchbar from './components/searchbar'
+import Searchbar from './components/Searchbar'
 import { ToastContainer, toast } from 'react-toastify';
 
 
